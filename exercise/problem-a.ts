fa9feb82eafc470954e1f9f4a6c28eee693a009b
Chao Ma <ynyounuo@me.com>
2019-04-04 12:16:38 -0700

    Finish Lab 1
    Signed-off-by: Chao Ma <ynyounuo@me.com>

--- exercise/problem-a.ts
+++ exercise/problem-a.ts
@@ -1,8 +1,26 @@
-// Given an array of ints a and an int k, return if there exists a pair (x, y) in a such that x + y = k.
+// Given an aay of ints a and an int k, return if there exists a pair (x, y) in a such that x + y = k.
 // This can be solved in O(nlog(n)) time trivially, or even in O(n) if you use another data structure
 
 function hasPairSumToK(a: number[], k: number): boolean {
-    
+    let results = [];
+    for (let i = 0; i < a.length; i++) {
+        for (let j = i + 1; j < a.length; j++) {
+            if (a[j] === k - a[i]) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+function hasPairSumToK2(a: number[], k: number): boolean {
+    const nums = new Set()
+    for (let i = 0; i < a.length; i++) {
+        if (nums.has(k - a[i])) {
+            return true
+        }
+    }
+    return false
 }
 
 
@@ -10,21 +28,21 @@ function hasPairSumToK(a: number[], k: number): boolean {
 
 
 // TESTS don't touch them
-function test(actual: [number[],number], expected:boolean){
-    if (hasPairSumToK(actual[0], actual[1]) != expected){
-        console.log('ERROR: hasPairSumToK(',actual[0] ,',', actual[1],') should be', expected)
+function test(actual: [number[], number], expected: boolean) {
+    if (hasPairSumToK(actual[0], actual[1]) != expected) {
+        console.log('ERROR: hasPairSumToK2(', actual[0], ',', actual[1], ') should be', expected)
     } else {
         console.log('All tests passed!')
     }
 }
 test([[], 2], false)
 
-test([[-1, -2, -3], -5],true);
+test([[-1, -2, -3], -5], true);
 test([[1, 2, 3, 4, 5, 7], 8], true);
 test([[1, 2, 3, 4, 96, -5], -4], true);
 
 
 test([[-1, -2, -3], 2], false);
 test([[1, 2, 3], 9], false);
-test([[7, 8, 9], 6],  false);
+test([[7, 8, 9], 6], false);
 
